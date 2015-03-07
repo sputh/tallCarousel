a7c99f957e877b5f103f4f0e823959b713655787
sputh <samantha.puth@gmail.com>
2015-03-06 17:56:31 -0800

    attempt to pass data as prop in react

--- widget/src/main.js
+++ widget/src/main.js
@@ -3,8 +3,10 @@ var test = {
 	'Title': 'Drag & Drop a Receipt',
 	'Url': 'http://support.usetallie.com/customer/portal/attachments/363862'
 }
-
-var Title = new React.createClass({
+/***********************************/
+// Header contains title and start //
+/***********************************/
+var Header = new React.createClass({
 	render: function() {
 		return React.createElement('div', {className: 'carousel-header'}, 
 			// this.props.title,
@@ -14,6 +16,9 @@ var Title = new React.createClass({
 	}
 })
 
+/***********************************/
+//    			Image Container        //
+/***********************************/
 var Img = new React.createClass({
 	render: function() {
 		return React.createElement('div', null, 
@@ -30,17 +35,23 @@ var Footer = new React.createClass({
 
 var Carousel = new React.createClass({
 	getInitialState: function() {
+		this.organizeImgs();
 		return {title: test.Title,
 						url  : test.Url}
 	},
 
+	organizeImgs: function() {
+		var keys = Object.keys(this.props)
+		console.log(this.props.data)
+		console.log(keys)
+	},
+
 	render: function() {
-		console.log(this.state.url)
 		return (
 			React.createElement('div', {className: 'carousel'}, 
 			React.createElement('div', {className: 'arrow-left'}), 
 			React.createElement('div', {className: 'carousel-content'}, 
-				React.createElement(Title, {title:  this.state.title}), 
+				React.createElement(Header, {title:  this.state.title}), 
 				React.createElement(Img, {url:  this.state.url}), 
 				React.createElement(Footer, null)
 			), 
