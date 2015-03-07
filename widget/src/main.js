5f6e5acc3672e1c9659d71dce19ef326711b1fed
sputh <samantha.puth@gmail.com>
2015-03-06 20:23:20 -0800

    Add click event handlers and fix certain namespacing

--- widget/src/main.js
+++ widget/src/main.js
@@ -18,40 +18,124 @@ var Header = new React.createClass({
 var Img = new React.createClass({
 	render: function() {
 		return React.createElement('div', null, 
-			React.createElement('img', {src: 'http://support.usetallie.com/customer/portal/attachments/363862'})
+			React.createElement('img', {src: this.props.url})
 		)
 	}
 })
 
+/***********************************/
+//    		Footer Container         //
+/***********************************/
 var Footer = new React.createClass({
 	render: function() {
-		return React.createElement('div', {className: 'carousel-footer'}, '1')
+		var images = this.props.images;
+		var context = this;
+		// return React.createElement('div', {className: 'carousel-footer'}, this.props.count)
+		return (
+			<div className="carousel-footer">
+			<ul> {Object.keys(images).map(function(result) {
+				var val = Math.abs(result - context.props.count-1)
+				return <li key={val} onClick={context.props.onClickIndex.bind(this, val)}>{val}</li>;
+			})}
+			</ul>
+			</div>
+		)
 	}
 })
 
+/***********************************/
+//   Carousel Parent Container     //
+/***********************************/
 var Carousel = new React.createClass({
 	getInitialState: function() {
+		this.props.images = data.Images;
 		this.organizeImgs();
-		return {title: test.Title,
-						url  : test.Url}
+		return {title: this.imagesCollection[1].title,
+						url  : this.imagesCollection[1].url,
+						index: 1}
 	},
 
 	organizeImgs: function() {
-		var keys = Object.keys(this.props)
-		console.log(this.props.data)
-		console.log(keys)
+		this.count = this.props.images.length;
+
+		// create a new associative array with increasing integer keys that organizes the images' information
+		this.imagesCollection = {};
+		var context = this;
+		this.props.images.forEach(function(val, key, arr) {
+			context.imagesCollection[key+1] = {
+				title: val.Title,
+				url: val.Url
+			}
+		})
+	},
+
+	leftClick: function(e) {
+		if (this.state.index ===1 ){
+			return;
+		} else {
+			var newIndex = this.state.index - 1;
+			this.setState({
+				index: newIndex,
+				title: this.imagesCollection[newIndex].title,
+				url: this.imagesCollection[newIndex].url
+			})
+		}
+	},
+
+	rightClick: function(e) {
+		if (this.state.index ===this.count ){
+			return;
+		} else {
+			var newIndex = this.state.index +1;
+			this.setState({
+				index: newIndex,
+				title: this.imagesCollection[newIndex].title,
+				url: this.imagesCollection[newIndex].url
+			})
+		}
+	},
+
+	onClickIndex: function(key) {
+		this.setState({
+				index: key,
+				title: this.imagesCollection[key].title,
+				url: this.imagesCollection[key].url
+		})
+	},
+
+	startSlideshow: function(e) {
+		this.setState({
+			index: 1,
+			title: this.imagesCollection[1].title,
+			url: this.imagesCollection[1].url
+		})
+
+		var context = this;
+		var slideshow = setInterval(function(){
+			var newIndex = context.state.index + 1;
+			if (newIndex > context.count) {
+				clearInterval(slideshow);
+			} else {			
+				context.setState({
+					index: newIndex,
+					title: context.imagesCollection[newIndex].title,
+					url: context.imagesCollection[newIndex].url
+				})
+			}
+		}, 3500)
 	},
 
 	render: function() {
 		return (
 			React.createElement('div', {className: 'carousel'}, 
-			React.createElement('div', {className: 'arrow-left'}), 
+			React.createElement('div', {className: 'arrow-left', onClick: this.leftClick}), 
 			React.createElement('div', {className: 'carousel-content'}, 
-				React.createElement(Header, {title:  this.state.title}), 
+				React.createElement(Header, {title:  this.state.title, startSlideshow: this.startSlideshow}), 
 				React.createElement(Img, {url:  this.state.url}), 
-				React.createElement(Footer, null)
+				React.createElement(Footer, {count: this.count, images: this.imagesCollection, onClickIndex: this.onClickIndex})
 			), 
-			React.createElement('div', {className: 'arrow-right'})
+			React.createElement('div',{className: 'right-box'}, React.createElement('div', {className: 'arrow-right', onClick: this.rightClick}))
+			
 			)
 		)
 	}
